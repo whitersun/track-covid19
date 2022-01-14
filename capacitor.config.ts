df23fc45f0bd1d9f4782a2a0ac0f236fa7d02f0c
whitersun <danghanqun0302@gmail.com>
2022-01-15 04:24:02 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.track.covid19',
+  appName: 'track-covid19',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
